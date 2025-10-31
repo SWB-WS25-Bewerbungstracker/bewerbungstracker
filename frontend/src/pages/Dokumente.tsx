667f5c8e460031ed356be62f154ee749cb10660c
JannikaBoerner <jaboit02@hs-esslingen.de>
2025-10-31 13:59:55 +0100

    Add files via upload

---
+++ frontend/src/pages/Dokumente.tsx
@@ -0,0 +1,10 @@
+const Dokumente: React.FC = () => {
+  return (
+    <div>
+      <h2>Dokumente</h2>
+      <p>Hier werden alle Dokumente angezeigt.</p>
+    </div>
+  );
+};
+
+export default Dokumente;
---
+++ frontend/src/pages/Termine.tsx
@@ -0,0 +1,10 @@
+const Termine: React.FC = () => {
+  return (
+    <div>
+      <h2>Termine</h2>
+      <p>Hier werden alle Termine angezeigt.</p>
+    </div>
+  );
+};
+
+export default Termine;
