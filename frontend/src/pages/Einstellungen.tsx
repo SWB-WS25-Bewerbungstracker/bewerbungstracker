355807eb41acbc1fc711dbfe5108ba6a9c7d8d0b
jaboit02 <jaboit02@hs-esslingen.de>
2025-11-05 19:23:14 +0100

    Frontend: Pages and Routing - src Folder updated

---
+++ frontend/src/pages/Einstellungen.tsx
@@ -0,0 +1,10 @@
+const Einstellungen: React.FC = () => {
+  return (
+    <div>
+      <h2>Einstellungen</h2>
+      <p>Hier werden alle Einstellungen angezeigt.</p>
+    </div>
+  );
+};
+
+export default Einstellungen;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Das wird die Startseite. Mit der Navigation Bar oben kann man auf die anderen Tabs navigieren.</p>
+    </div>
+  );
+};
+
+export default Home; 
