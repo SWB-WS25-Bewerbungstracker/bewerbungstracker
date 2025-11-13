8a45b5f49bc0656d091909a43a3ee394da673b37
relunt <lureit02@hs-esslingen.de>
2025-11-13 13:06:51 +0100

    feat[backend]: Schnittstelle für Firmenname und ID rausgelegt unter .../companies

---
+++ backend/src/main/java/com/bewerbungstracker/dto/CompanyDTO.java
@@ -0,0 +1,11 @@
+package com.bewerbungstracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CompanyDTO {
+    private Integer id;
+    private String companyname;
+}
