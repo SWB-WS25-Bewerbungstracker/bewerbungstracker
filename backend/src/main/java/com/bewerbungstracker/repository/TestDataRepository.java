52686891b01d6e1f70f45d77027d1a142d2f43bf
niolee <lahisg00@hs-esslingen.de>
2025-11-16 15:27:48 +0100

    fix: remove testdata setup
    Signed-off-by: niolee <lahisg00@hs-esslingen.de>

--- backend/src/main/java/com/bewerbungstracker/repository/TestDataRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bewerbungstracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.bewerbungstracker.entity.TestData;
-
-public interface TestDataRepository extends JpaRepository<TestData, Long> {}
