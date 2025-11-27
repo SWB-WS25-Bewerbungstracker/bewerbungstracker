41d0b5a0fda486a34a2c593f7d7e7357e37a13dc
niolee <lahisg00@hs-esslingen.de>
2025-11-27 23:37:12 +0100

    fix[api]: remove trailing slash from origin
    -> avoid run-in with potential issues in the future
    
    Signed-off-by: niolee <lahisg00@hs-esslingen.de>

--- backend/src/main/java/com/bewerbungstracker/singlejobofferview/SingleJobofferViewController.java
+++ backend/src/main/java/com/bewerbungstracker/singlejobofferview/SingleJobofferViewController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173/", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RequestMapping("/joboffer")
 @RequiredArgsConstructor
 public class SingleJobofferViewController {
