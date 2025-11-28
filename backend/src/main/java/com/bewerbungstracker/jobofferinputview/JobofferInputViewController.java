af7574d5da32d35248c7fde5564e0ebd1e09dc2a
relunt <lureit02@hs-esslingen.de>
2025-11-28 11:18:35 +0100

    feat: close input page after success

--- backend/src/main/java/com/bewerbungstracker/jobofferinputview/JobofferInputViewController.java
+++ backend/src/main/java/com/bewerbungstracker/jobofferinputview/JobofferInputViewController.java
@@ -3,7 +3,9 @@ package com.bewerbungstracker.jobofferinputview;
 import com.bewerbungstracker.entity.Joboffer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -15,9 +17,10 @@ public class JobofferInputViewController {
     private final JobofferInputViewService jobofferInputViewService;
 
     @PostMapping(path = "/inputForm", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String postJobofferInfo(@RequestBody JobofferInputDTO jobofferInfo) {            //should return success page
+    public ResponseEntity<String> postJobofferInfo(@RequestBody JobofferInputDTO jobofferInfo) {            //should return success page
         log.debug("jobofferInfo: {}", jobofferInfo);
-        Joboffer test = jobofferInputViewService.saveJobofferInput(jobofferInfo);
-        return test.toString();
+        jobofferInputViewService.saveJobofferInput(jobofferInfo);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Bewerbung erfolgreich erstellt!");
     }
 }
