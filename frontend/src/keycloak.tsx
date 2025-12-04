f871689c0e5fad517ec68fa003a5b93d4699d52c
magor362 <magor@klein-network.de>
2025-12-04 23:37:32 +0100

    fix: keycloak utulizes now localhost instead of authentication

--- frontend/src/keycloak.tsx
+++ frontend/src/keycloak.tsx
@@ -1,9 +1,9 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
- url: "http://authentication:7080",
- realm: "bt-realm",
- clientId: "bt-client",
+    url: "http://localhost:7080",
+    realm: "bt-realm",
+    clientId: "bt-client",
 });
 
 
