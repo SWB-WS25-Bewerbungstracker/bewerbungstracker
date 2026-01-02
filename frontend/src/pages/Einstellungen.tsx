5217096c29d5cbe3dcf65b162e3854b14bc16410
nomuit00 <norman.muenzer@googlemail.com>
2026-01-02 21:17:49 +0100

    feat: change settings menu to drawer and move drawer into separate file

--- frontend/src/pages/Einstellungen.tsx
+++ frontend/src/pages/Einstellungen.tsx
@@ -1,10 +1,10 @@
 const Einstellungen: React.FC = () => {
-  return (
-    <div>
-      <h2>Einstellungen</h2>
-      <p>Hier werden alle Einstellungen angezeigt.</p>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Profil</h2>
+            <p>Hier werden Einstellungen angezeigt.</p>
+        </div>
+    );
 };
 
 export default Einstellungen;
